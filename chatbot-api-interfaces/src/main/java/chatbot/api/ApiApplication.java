b4216259b3dcf629729ab858add5efbb3cbaec37
Shuo <1070465541@qq.com>
2025-04-14 20:59:20 +0800

    Shuo,feat:init ddd project

---
+++ chatbot-api-interfaces/src/main/java/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shuo
+ * @description 启动入口
+ * @github <a href="https://github.com/Shuo712">
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package chatbot.api.test;
+
+/**
+ * @author Shuo
+ * @description 单元测试
+ * @github <a href="https://github.com/Shuo712">
+ */
+public class ApiTest {
+}
